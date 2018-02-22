b3de70240fa376aca6fa65db73a2fcdb10e03163
Daniel David <danieldavid@dcc.ufba.br>
2018-02-22 10:46:24 -0300

    Ajustando botoes na tela inicial

--- CRUD_PHP/index.php
+++ CRUD_PHP/index.php
@@ -6,44 +6,46 @@
 	<link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 
+</style>
+
 <body>
 
 	<header>
 		<h1>Turismo</h1>
 	</header>
 	<center>
-		<table align="center" width="40%" height="40%">
+		<table>
 			<tr>
-				<td>
+				<td width="30%" height="40%">
 					<a href="pages/clientes.php" class="btn blue">Clientes<br><br>
 						<img src="imgs/i-clientes3.png" height="70" width="70">
 					</a>
 				</td>
-				<td>
+				<td width="30%" height="40%">
 					<a href="pages/anunciantes.php" class="btn blue">Anunciantes<br><br>
 						<img src="imgs/i-anunc.png" height="70" width="70">
 					</a>
 				</td>
-				<td>
+				<td width="30%" height="40%">
 					<a href="pages/cidades.php" class="btn blue">Cidades<br><br>
 						<img src="imgs/i-brasil.png" height="70" width="70">
 					</a>
 				</td>
 			</tr>
 			<tr>
-				<td>
+				<td width="30%" height="40%">
 					<a href="pages/eventos.php" class="btn blue">Eventos<br><br>
 						<img src="imgs/i-eventos.png" height="70" width="70">
 					</a>
 				</td>
-				<td>
+				<td width="30%" height="40%">
 					<a href="pages/hospedagens.php" class="btn blue">Hospedagens<br><br>
 						<img src="imgs/i-hoteis.png" height="70" width="70">
 					</a>
 				</td>
-				<td>
+				<td width="30%" height="40%">
 					<a href="pages/pontos_turisticos.php" class="btn blue">Pontos Turísticos<br><br>
-					<img src="imgs/i-pturist.png" height="70" width="70">
+					<img src="imgs/i-pturist.png" height="60" width="60">
 					</a>
 				</td>
 			</tr>
